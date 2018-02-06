4d85f71c72cf1da41ace810fcf5291607f7f879c
ZhiyangLeeCN <zhiyangleeCN@gmail.com>
2018-02-06 23:33:52 +0800

    A*算法初稿

--- A-Star Search Algorithm/main.cpp
+++ A-Star Search Algorithm/main.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <vector>
+#include <set>
+#include <stack>
 #include <string>
 
+using std::vector;
+using std::set;
+using std::stack;
 using std::string;
 
 class GraphNode {
@@ -11,17 +17,19 @@ public:
 
 	GraphNode() :
 		is_obstacle_(false),
+		is_closed_(false),
 		row_index_(0),
 		col_index_(0),
-		f(0),
-		g(0),
-		h(0),
+		f(UINT_MAX),
+		g(UINT_MAX),
+		h(UINT_MAX),
 		display_mark_("-"),
 		pervNode(nullptr)
 	{
-	};
+	}
 
 	bool   is_obstacle_;
+	bool   is_closed_;
 	size_t row_index_;
 	size_t col_index_;
 	size_t f;
@@ -52,7 +60,23 @@ public:
 				this->PutNode(nullptr, i, j);
 			}
 		}
-	};
+	}
+
+	~Graph()
+	{
+		for (size_t i = 0; i < rows_; i++)
+		{
+			for (size_t j = 0; j < cols_; j++)
+			{
+				GraphNode *node = this->FindByIndex(i, j);
+				if (nullptr != node) {
+					delete node;
+				}
+			}
+		}
+
+		delete graph_;
+	}
 
 	void Print()
 	{
@@ -83,7 +107,7 @@ public:
 		}
 		printf("\n");
 
-	};
+	}
 
 	void PutNode(GraphNode *graphNode)
 	{
@@ -96,7 +120,7 @@ public:
 
 		this->SetLocation(graphNode, graphNode->row_index_, graphNode->col_index_);
 
-	};
+	}
 
 	void PutNode(GraphNode *graphNode, size_t row_index, size_t col_index)
 	{
@@ -111,6 +135,136 @@ public:
 
 		this->SetLocation(graphNode, row_index, col_index);
 
+	}
+
+	size_t estimatedDistance(GraphNode *currentNode, GraphNode *dstNode)
+	{
+		long long cur_row_index = currentNode->row_index_;
+		long long cur_col_index = currentNode->col_index_;
+
+		long long dst_row_index = dstNode->row_index_;
+		long long dst_col_index = dstNode->col_index_;
+
+		return std::abs(cur_row_index - dst_row_index) + std::abs(cur_col_index - dst_col_index);
+	}
+
+	//A* serach
+	vector<GraphNode *> *FindPath(GraphNode *srcNode, GraphNode *dstNode)
+	{
+
+		bool reached = false;
+		set<GraphNode *> openList;
+		
+		srcNode->f = 0;
+		srcNode->g = 0;
+		srcNode->h = 0;
+		openList.insert(srcNode);
+
+		while (!openList.empty())
+		{
+			GraphNode *currentNode = *openList.begin();
+			openList.erase(openList.begin());
+
+			currentNode->display_mark_ = "S";
+			currentNode->is_closed_ = true;
+
+			//find target
+			if (currentNode->col_index_ == dstNode->col_index_ && 
+				currentNode->row_index_ == dstNode->row_index_) {
+				reached = true;
+				break;
+			}
+
+			vector<GraphNode *> *neighbors = this->GetNeighborsByNode(currentNode);
+			if (!neighbors->empty()) {
+
+				for (auto iter = neighbors->begin(); iter != neighbors->end(); iter++)
+				{
+					GraphNode *nNode = *iter;
+					size_t nG = currentNode->g + 1;
+					size_t nH = this->estimatedDistance(nNode, dstNode);
+					size_t nF = nG + nH;
+
+					if (nNode->f > nF) {
+
+						nNode->f = nF;
+						nNode->g = nG;
+						nNode->h = nH;
+						nNode->pervNode = currentNode;
+
+						openList.insert(nNode);
+
+					}
+				}
+
+			}
+
+			delete neighbors;
+		}
+
+		vector<GraphNode *> *pathList = new vector<GraphNode *>;
+		if (reached) {
+
+			stack<GraphNode *> pathStack;
+			GraphNode *iterNode = dstNode;
+			while (nullptr != iterNode)
+			{
+				pathStack.push(iterNode);
+				iterNode = iterNode->pervNode;
+			}
+
+			while (!pathStack.empty())
+			{
+				pathList->push_back(pathStack.top());
+				pathStack.pop();
+			}
+
+		}
+
+		return pathList;
+
+	}
+
+	vector<GraphNode *> *GetNeighborsByNode(GraphNode *node)
+	{
+		vector<GraphNode *> filter;
+		vector<GraphNode *> *result = new vector<GraphNode *>;
+
+		//вС
+		if (node->col_index_ > 0) {
+			GraphNode *leftNode = this->FindByIndex(node->row_index_, node->col_index_ - 1);
+			filter.push_back(leftNode);
+		}
+
+		//ср
+		if (node->col_index_ < cols_ - 1) {
+			GraphNode *rightNode = this->FindByIndex(node->row_index_, node->col_index_ + 1);
+			filter.push_back(rightNode);
+		}
+
+		//ио
+		if (node->row_index_ > 0) {
+			GraphNode *topNode = this->FindByIndex(node->row_index_ - 1, node->col_index_);
+			filter.push_back(topNode);
+		}
+
+		//об
+		if (node->row_index_ < rows_ - 1) {
+			GraphNode *bottomNode = this->FindByIndex(node->row_index_ + 1, node->col_index_);
+			filter.push_back(bottomNode);
+		}
+
+		for (auto iter = filter.begin(); iter != filter.end(); iter++)
+		{
+			GraphNode *iterNode = *iter;
+			if (nullptr != iterNode && 
+				!iterNode->is_closed_ && 
+				!iterNode->is_obstacle_) {
+				result->push_back(iterNode);
+			}
+		}
+
+		return result;
 	}
 
 	GraphNode *FindByIndex(size_t row_index, size_t col_index)
@@ -135,6 +289,16 @@ public:
 		}
 	}
 
+	size_t GetRows()
+	{
+		return rows_;
+	}
+
+	size_t GetCols()
+	{
+		return cols_;
+	}
+
 private:
 	size_t rows_;
 	size_t cols_;
@@ -156,12 +320,33 @@ private:
 int main() {
 
 	Graph *graph = new Graph;
-	GraphNode *node = new GraphNode;
-	graph->PutNode(node);
+
+	for (size_t i = 0; i < graph->GetRows(); i++)
+	{
+		for (size_t j = 0; j < graph->GetCols(); j++)
+		{
+			graph->PutNode(new GraphNode, i, j);
+		}
+	}
+
+	graph->Print();
+
+	GraphNode *src = graph->FindByIndex(0, 0);
+	GraphNode *dst = graph->FindByIndex(7, 7);
+
+	vector<GraphNode *> *pathList = graph->FindPath(src, dst);
+	for (auto iter = pathList->begin(); iter != pathList->end(); iter++)
+	{
+		GraphNode *node = *iter;
+		node->display_mark_ = "*";
+	}
 
 	graph->Print();
 
 	getchar();
+
+	delete pathList;
+	delete graph;
 
 	return 0;
 
